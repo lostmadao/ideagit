7a891e03074d5e75a77aecb4861533dedb23f6d7
lostmadao <lostmadao@yeah.net>
2019-03-25 09:39:01 +0800

    新建user.java,提交到本地仓库

---
+++ git_day01/src/main/java/com/yinzheng/pojo/User.java
@@ -0,0 +1,41 @@
+package com.yinzheng.pojo;
+
+public class User {
+
+    private String username;
+    private Integer age;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                ", age=" + age +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
